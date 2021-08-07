15d47abd6a794999dd87e6590c662e6fa14966e3
Dmitriy <nikitin@uteka.ru>
2021-08-08 00:20:26 +0300

    QA-8 add test add service

---
+++ pullcrm-cypress/cypress/integration/main_actions/add_service.js
@@ -0,0 +1,9 @@
+let ds = require('../../storage/dataStorage.json');
+it(`add service`, function() {
+    cy.deleteUser(ds.regNewUser.tel)
+    cy.visitAuth(Cypress.env('CY_BASE_URL'));
+    cy.registrationUser(ds.regNewUser.tel, ds.regNewUser.pass, ds.regNewUser.name);
+    cy.regNewCompany('Gorshochek', 'Киев', 'Barbershop', 2);
+    cy.addService('Шикарная стрижка', '142', '15', 'Будет шикарно');
+    cy.addService('Хреновая стрижка', '42', '30', 'Будет нормально');
+});
