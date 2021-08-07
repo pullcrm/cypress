f6039cceb2d20d2e10c7aec36bb4ee946bf0aee1
Dmitriy <nikitin@uteka.ru>
2021-08-07 17:47:24 +0300

    add test create new company

---
+++ pullcrm-cypress/cypress/integration/create/create_new_company.js
@@ -0,0 +1,8 @@
+let ds = require('../../storage/dataStorage.json');
+it(`Create new company`, function() {
+    cy.deleteUser(ds.regNewUser.tel)
+    cy.visitAuth(Cypress.env('CY_BASE_URL'));
+    cy.registrationUser(ds.regNewUser.tel, ds.regNewUser.pass, ds.regNewUser.name)
+    cy.regNewCompany('BestOfTheBest', 'Киев', 'Barbershop', 2)
+    cy.regNewCompany('Подгоршек', 'Черновцы', 'Салон красоты', 3, true)
+});
---
+++ pullcrm-cypress/cypress/support/commands/new_company.js
@@ -0,0 +1,30 @@
+let element = require('../../storage/dataTest.json');
+
+Cypress.Commands.add('regNewCompany', (companyName, city, categoryName, num, isDashbord) => {
+    if (isDashbord === true) {
+        cy.get(element.carouselItem)
+            .contains('Добавить компанию')
+            .click();
+    }
+    cy.contains('Создание компании');
+    cy.get(element.inputCompanyName)
+        .type(companyName);
+    cy.get(element.inputCityName)
+        .click();
+    cy.get(element.selectBody)
+        .contains(city)
+        .click();
+    cy.get(element.inputCategoryName)
+        .click();
+    cy.get(element.selectBody)
+        .contains(categoryName)
+        .click();
+    cy.get(element.submitBtn)
+        .click();
+    cy.get(element.carouselItem)
+        .contains(companyName)
+    cy.get(element.carouselItem).then(carouselItem => {
+        console.log(carouselItem.length)
+        assert.isOk(carouselItem.length === num, `На экране отображаются созданные компании и блок "Добавить компанию"`)
+    });
+});
