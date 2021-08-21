2a4a8419548f7d2ff18ad43eda1b35d07fd47579
Dmitriy <nikitin@uteka.ru>
2021-08-21 23:46:15 +0300

    add test add profile photo qa-10

--- pullcrm-cypress/cypress/support/commands/specialists.js
+++ pullcrm-cypress/cypress/support/commands/specialists.js
@@ -1,5 +1,7 @@
 let element = require('../../storage/dataTest.json');
-
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+}); //отключить отслеживание ошибок в консоле
 Cypress.Commands.add('regNewSpecialists', (companyName, specialists) => {
     console.log(specialists)
     cy.get(element.navbarDesktop)
@@ -21,4 +23,29 @@ Cypress.Commands.add('regNewSpecialists', (companyName, specialists) => {
         .type(specialists.pass).wait(1000)
     cy.get(element.specialistsPageCard)
         .contains(specialists.name)
+});
+
+Cypress.Commands.add('addProfilePhoto', (filepath) => {
+    cy.get(element.navbarDesktop)
+        .find(element.specialistsLink)
+        .click();
+    cy.get(element.specialistsPageCardEdit)
+    cy.get(element.specialistsPageCard)
+        .find('img')
+        .should('not.exist');
+    cy.get(element.specialistsPageCardEdit)
+        .click();
+    cy.get(element.specialistAboutPageAvatars)
+        .find('img')
+        .should('not.exist');
+    cy.get(element.inputFile)
+        .attachFile(filepath).wait(1000)
+    cy.get(element.specialistAboutPageAvatars)
+    cy.get('img')
+        .should('be.visible');
+    cy.get(element.back)
+        .click();
+    cy.get(element.specialistsPageCard)
+        .find('img')
+        .should('be.visible');
 });
